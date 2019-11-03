bc9a348c2a0b99a5bd81cf6ecd0be95a22886bfd
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-11-02 21:03:56 -0700

    Attempt with new token

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Faster
+          Learn React Faster And Faster
         </a>
       </header>
     </div>
