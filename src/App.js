5af4fe29de4e2a4de216cd15d726b3863cc39277
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-11-02 22:32:09 -0700

    Edit copy

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Faster And Faster
+          Learn React Faster And Faster And Faster
         </a>
       </header>
     </div>
