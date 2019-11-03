8b4e7e24eef8db6399d635ec1cbab6cb133d739d
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-11-02 18:33:14 -0700

    Really test Now on push

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Fast
+          Learn React Faster
         </a>
       </header>
     </div>
